6f7ecb3606e8445bbc18936e687558b31cac211f
tempkh <55805988+tempkh@users.noreply.github.com>
2019-09-25 19:49:21 +0200

    get all beers

--- js/app.js
+++ js/app.js
@@ -7,6 +7,20 @@ new Vue ({
 	
 function getBeers() {
 	
-	return 'beer list';
+	beers = [];
 	
+	$.ajax({
+	    url: 'https://api.punkapi.com/v2/beers',
+        success: function(data){
+            
+			data.forEach(function(beer) {
+			  console.log(beer.name);
+				beers.push(beer.name);
+			});
+			
+        }
+	})
+
+	
+	return beers;
 }
